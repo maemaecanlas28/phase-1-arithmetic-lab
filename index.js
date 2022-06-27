cb2c218332e3996a8145d104328e8b3f2254f8fa
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-06-27 16:24:58 -0700

    commit tree 6adb5a0cb2a007516d40d340ee9a0178f370a6e9

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2 
+
+let random = 0;
+
+random ++;
+
+let num3 = 24;
+let num4 = 20;
+const mod = num3 % num4;
+
+const numbers = new Set ([5,4,3,20,17,18]);
+const max = Math.max(...numbers);
